ace7ef02c27d2dc7a423197869dfb74925f424cf
connorwalsh <c@polygon.pizza>
2019-07-15 18:00:49 -0400

    modify main js process to use an OSC UDP transport to backend

---
+++ README.md
@@ -0,0 +1,12 @@
+# op
+fm synthesizer vst
+
+
+# architecture
+* rust backend
+* electron gui
+* gui <--> backend interop over UDP via OSC
+
+# notes
+* [using `rosc`](https://github.com/MikeSperone/chord_arp_rs/blob/master/src/main.rs)
+* [using `osc.js` installation](https://serialport.io/docs/guide-installation#electron)
--- gui/main_process.js
+++ gui/main_process.js
@@ -3,6 +3,7 @@ const {app, BrowserWindow, ipcMain} = electron
 
 const spawn = require('child_process').spawn
 const net = require('net')
+const osc = require('osc')
 
 
 // Let electron reloads by itself when webpack watches changes in ./app/
@@ -10,16 +11,24 @@ require('electron-reload')(__dirname)
 
 // To avoid being garbage collected
 let mainWindow
-let synthClient
+let updClient
 
-const SynthBackend = {
-  host: 'localhost',
+const UDPClient = {
+  host: '0.0.0.0',
+  port: 57121,
+}
+
+const UDPBackend = {
+  host: '127.0.0.1',
   port: 3333,
 }
 
 const DefaultWindowConfig = {
   width: 800,
   height: 600,
+  webPreferences: {
+    nodeIntegration: true, 
+  }
 }
 
 const RENDER_TO_MAIN_IPC_CHAN = 'update'
@@ -30,17 +39,25 @@ spawn('cargo', ['run', '..'])
 app.on('ready', () => {
 
   // create socket to backend process
-  synthClient = net.Socket()
+  // synthClient = net.Socket()
 
-  // if there is an error connecting, retry connecting
-  synthClient.on('error', (error) => {
-    synthClient.connect(SynthBackend);  
+  // create a UDP OSC client
+  udpClient = new osc.UDPPort({
+    localAddress: UDPClient.host,
+    localPort: UDPClient.port,
+    metadata: true
+  })
+
+  // Listen for incoming OSC bundles.
+  udpClient.on("bundle", (oscBundle, timeTag, info) => {
+    console.log("An OSC bundle just arrived for time tag", timeTag, ":", oscBundle);
+    console.log("Remote info is: ", info);
   })
   
-  // attempt to connect
-  synthClient.connect(SynthBackend);
+  // open client UDP port
+  udpClient.open()
 
-  synthClient.on('connect', () => {
+  udpClient.on("ready", () => {
     // we are now connected to the synth backend
     
     // setup main windows
@@ -48,7 +65,8 @@ app.on('ready', () => {
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
     // listen for responses from backend and send directly to renderer process
-    synthClient.on('data', (data) => {
+    // TODO fix this to receive "bundle" messages like above!
+    udpClient.on('data', (data) => {
       // parse the incoming data to send the updates to the appropriate component
       // each incoming message should be a JSON object where the top level keys are the
       // ids of the components which need to be updated.
@@ -57,12 +75,26 @@ app.on('ready', () => {
         mainWindow.webContents.send(component, JSON.stringify(jsonData[component])) 
       }
     })
-    
+
     // listen for incoming ipc from renderer process
     ipcMain.on(RENDER_TO_MAIN_IPC_CHAN, (event, data) => {
       // on each incoming message, relay directly to the synth backend.
-      synthClient.write(data)
-    })    
+      // synthClient.write(data)
+      updClient.send({
+        address: "/s_new",
+        args: [
+          {
+            type: "s",
+            value: "default"
+          },
+          {
+            type: "i",
+            value: 100
+          }
+        ]
+      }, UDPBackend.host, UDPBackend.port)
+    })
   })
+  
 })
 
