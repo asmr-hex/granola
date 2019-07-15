6e3d678fbb0e876b557e83243b7fea4e396eb03f
connorwalsh <c@polygon.pizza>
2019-07-15 18:17:34 -0400

    begin converting rust server from tcp -> udp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::thread;
-use std::net::{TcpListener, TcpStream, Shutdown};
+use std::net::{UdpSocket, SocketAddrV4};
 use std::io::{Read, Write};
 
 fn handle_client(mut stream: TcpStream) {
@@ -18,25 +18,46 @@ fn handle_client(mut stream: TcpStream) {
     } {}
 }
 
+fn listen(socket: &net::UdpSocket, mut buffer: &mut [u8]) -> usize {
+
+    let (number_of_bytes, src_addr) = socket.recv_from(&mut buffer).expect("no data received");
+
+    println!("{:?}", number_of_bytes);
+    println!("{:?}", src_addr);
+
+    // do more stuff
+
+    number_of_bytes
+}
+
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
-    // accept connections and process them, spawning a new thread for each one
-    println!("Server listening on port 3333");
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move|| {
-                    // connection succeeded
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
-            }
+    let socket = UdpSocket::bind("0.0.0.0:3333").expect("failed to bind to host socket!");
+    
+    let mut buf: Vec<u8> = Vec::with_capacity(100);
+    loop {
+        while listen(&socket, &mut buf != 0) {
+            
         }
     }
-    // close the socket server
-    drop(listener);
+
+    // TODO: do something like this and spawn a new thread to handkle the incoming request!
+    // let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
+    // // accept connections and process them, spawning a new thread for each one
+    // println!("Server listening on port 3333");
+    // for stream in listener.incoming() {
+    //     match stream {
+    //         Ok(stream) => {
+    //             println!("New connection: {}", stream.peer_addr().unwrap());
+    //             thread::spawn(move|| {
+    //                 // connection succeeded
+    //                 handle_client(stream)
+    //             });
+    //         }
+    //         Err(e) => {
+    //             println!("Error: {}", e);
+    //             /* connection failed */
+    //         }
+    //     }
+    // }
+
 }
